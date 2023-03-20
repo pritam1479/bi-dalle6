48d32a5beae06bdf3af00aada9de38e66d599a67
Pritam <pritammondal1479@gmail.com>
2023-03-20 20:28:01 +0530

    fourth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -53,7 +53,7 @@ function App() {
 
       // Save user to MongoDB
       if (profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://bi-dalle6.onrender.com/api/v1/users', {
           method: 'POST',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -124,7 +124,7 @@ function App() {
         <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
         <RefineSnackbarProvider>
           <Refine
-            dataProvider={dataProvider("http://localhost:8080/api/v1")}
+            dataProvider={dataProvider("https://bi-dalle6.onrender.com/api/v1")}
             notificationProvider={notificationProvider}
             ReadyPage={ReadyPage}
             catchAll={<ErrorComponent />}
